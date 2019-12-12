a94e8fe1c028ec6283bc07e0dbea21bfe0f5084e
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2019-12-12 20:57:20 +0000

    super user added

--- reserve_system_ece/urls.py
+++ reserve_system_ece/urls.py
@@ -26,3 +26,39 @@ urlpatterns = [
     path('signin/oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('docs/' , include_docs_urls(title= 'Reserve System Documentation')),
 ]
+
+
+
+
+'''
+send a packet with post to "localhost:8000/signin/oauth/token" for LOGIN:
+
+
+request:
+            {
+                "grant_type" : "password",
+                "client_id" : "IzAwTAgizQfD59DJwF8JNfNNpi6axwENo5Eltk3z", # from oauth/applications
+                "username" : "my_username",
+                "password" : "my_password"
+            }
+
+response:
+            {
+                "access_token": "WDZkCMO7Ah5Y96va0Pog9OkTFWET7O",
+                "expires_in": 36000,
+                "token_type": "Bearer",
+                "scope": "read write groups",
+                "refresh_token": "LWuJaxNJVxKcHz7qJneXrNS9Fm0RoL"
+            }
+
+getting a new token:
+
+            {
+                "grant_type" : "refresh_token",
+                "client_id" : "IzAwTAgizQfD59DJwF8JNfNNpi6axwENo5Eltk3z",  # from oauth/applications
+                "refresh_token": "LWuJaxNJVxKcHz7qJneXrNS9Fm0RoL"
+            }
+
+LOGIN URL: localhost:8000/signin/oauth/token
+
+'''
